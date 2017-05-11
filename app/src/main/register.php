4b79f2630cdad8e68138a4a4b56c4c25e9cb7f39
Peter Corletto <peter.corletto@gmail.com>
2017-05-11 16:16:26 -0400

    Updated register.php

--- app/src/main/register.php
+++ app/src/main/register.php
@@ -1,6 +1,6 @@
 <?php
 require "init.php";
-$clientid=$_POST["clientid"];
+
 $username=$_POST["username"];
 $password=$_POST["password"];
 $firstname=$_POST["firstname"];
@@ -13,17 +13,25 @@ $zipcode=$_POST["zipcode"];
 $telephone=$_POST["telephone"];
 $mobile=$_POST["mobile"];
 
-$sql_query="insert into clientTable values('$clientid','$username','$password','$firstname','$lastname','$email','$address','$city','$state','$zipcode','$telephone','$mobile');";
+$sql_query = "select username from clientTable where username = '$username';";
+ $result = mysqli_query($con,$sql_query);  
+ if(mysqli_num_rows($result) >0 )  
+ {  
+//$row = mysqli_fetch_assoc($result); 
+//echo var_dump($row);
+echo "That username already exists";
+}
 
+else{
+$clientid=$_POST["clientid"];
+$sql_query="insert into clientTable values('$clientid','$username','$password','$firstname','$lastname','$email','$address','$city','$state','$zipcode','$telephone','$mobile');";
 if(mysqli_query($con,$sql_query))
 {
 //echo "<h3>Data insertion success...</h3>";
 }
-
 else
 {
 //echo "Data insertion error...".mysqli_error($con);
 }
-
-
+}
 ?>
